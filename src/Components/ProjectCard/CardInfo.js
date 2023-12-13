c02471f57d88feb30b8302faf92f940818a0c313
gauravtripathii <gauravtripathii>
2023-12-14 00:20:59 +0530

    extra challenge space added

--- src/Components/ProjectCard/CardInfo.js
+++ src/Components/ProjectCard/CardInfo.js
@@ -7,63 +7,86 @@ function CardInfo() {
       id: 1,
       imageUrl: "./assets/Home/event/music.svg",
       title: "Music",
-      link:"/music",
+      link: "/music",
     },
     {
       id: 2,
       imageUrl: "./assets/Home/event/dance.svg",
       title: "Dance",
-      link:"/dance",
+      link: "/dance",
     },
     {
       id: 3,
       imageUrl: "./assets/Home/event/draw.svg",
       title: "Painting",
-      link:"/painting",
+      link: "/painting",
     },
     {
       id: 4,
       imageUrl: "./assets/Home/event/acting.svg",
       title: "Acting",
-      link:"/acting",
+      link: "/acting",
     },
     {
       id: 5,
       imageUrl: "./assets/Home/event/graphic.svg",
       title: "Design",
-      link:"/design",
+      link: "/design",
     },
     {
       id: 6,
       imageUrl: "./assets/Home/event/photography.svg",
       title: "Snapshot",
-      link:"/snapshot",
+      link: "/snapshot",
     },
+  ]);
+  const [b_last] = useState([
     {
       id: 7,
       imageUrl: "./assets/Home/event/Fashion.svg",
       title: "Fashion",
-      link:"/fashion",
+      link: "/fashion",
     },
     {
       id: 8,
       imageUrl: "./assets/Home/event/video.svg",
       title: "Short Flim",
-      link:"/flim",
+      link: "/flim",
     },
     {
       id: 9,
       imageUrl: "./assets/Home/event/writing.svg",
       title: "Writing",
-      link:"/writing",
+      link: "/writing",
     },
-    
+    {
+      id: 10,
+      imageUrl: "./assets/Home/event/writing.svg",
+      title: "Writing",
+      link: "/writing",
+    },
   ]);
   return (
-    <div className="overflow-hidden w-full flex justify-center" id="cards">
-      <div className="mb-10 w-10/12 flex flex-wrap justify-center">
+    <div
+      className="overflow-hidden w-full flex flex-col justify-center items-center"
+      id="cards"
+    >
+      <div className="w-10/12 flex flex-wrap justify-center">
         {b.map(({ id, ...otherprops }) => (
-          <div key={id} className="w-10/12 md:w-6/12 lg:w-4/12 z-20 flex justify-center">
+          <div
+            key={id}
+            className="w-10/12 md:w-6/12 lg:w-4/12 z-20 flex justify-center"
+          >
+            <CardStyle key={id} {...otherprops} />
+          </div>
+        ))}
+      </div>
+      <div className="mb-10 w-10/12 flex flex-wrap justify-center">
+        {b_last.map(({ id, ...otherprops }) => (
+          <div
+            key={id}
+            className="w-10/12 md:w-6/12 lg:w-3/12 z-20 flex justify-center"
+          >
             <CardStyle key={id} {...otherprops} />
           </div>
         ))}
