5b2e6fd5c7233899c940ad0741945024caa0989c
gauravtripathii <gauravtripathii>
2023-12-13 23:01:23 +0530

    fixed -> element on top of header button

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,16 +1,14 @@
-import React, {useState, useEffect} from "react";
+import React, { useState, useEffect } from "react";
 import Header from "../Components/Home/Header";
 import Hero from "../Components/Home/Hero";
 
 function Home() {
   const [height, setHeight] = useState();
-  useEffect(()=> setHeight(document.body.scrollHeight));
+  useEffect(() => setHeight(document.body.scrollHeight));
   return (
-    <div className="flex flex-col" style={{height: `${height}px`}}>
+    <div className="flex flex-col relative" style={{ height: `${height}px` }}>
       <Header />
-      <div className="flex-1 relative">
-      <Hero />
-      </div>
+        <Hero />
     </div>
   );
 }
